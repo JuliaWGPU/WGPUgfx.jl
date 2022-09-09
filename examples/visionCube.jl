fbc67804e734bf49ea5365565e5f36b6f6d86aae
arhik <arhik23@gmail.com>
2022-09-09 20:36:15 +0530

    Adding incomplete vision example
    Still needs work on vision but this code may not
    change. so going ahead adding this

---
+++ examples/visionCube.jl
@@ -0,0 +1,49 @@
+using Revise
+using Debugger
+using WGPUgfx
+using WGPU
+using WGPU_jll
+using GLFW
+using GLFW: WindowShouldClose, PollEvents, DestroyWindow
+using LinearAlgebra
+using Rotations
+using StaticArrays
+
+WGPU.SetLogLevel(WGPU.WGPULogLevel_Debug)
+canvas = WGPU.defaultCanvas(WGPU.WGPUCanvas);
+gpuDevice = WGPU.getDefaultDevice();
+
+camera = defaultVision()
+light = defaultLighting()
+
+scene = Scene(
+	gpuDevice, 
+	canvas, 
+	camera, 
+	light, 
+	[], 
+	repeat([nothing], 6)...
+)
+
+mesh = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "cube.obj"))
+
+addObject!(scene, mesh)
+
+attachEventSystem(scene)
+
+main = () -> begin
+	try
+		while !WindowShouldClose(canvas.windowRef[])
+			runApp(gpuDevice, scene)
+			PollEvents()
+		end
+	finally
+		WGPU.destroyWindow(canvas)
+	end
+end
+
+if abspath(PROGRAM_FILE)==@__FILE__
+	main()
+else
+	main()
+end
