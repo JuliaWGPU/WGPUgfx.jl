e5ab8165535f11ae1281cebe851acec003a01c5c
arhik <arhik23@gmail.com>
2022-09-09 20:35:18 +0530

    changed examples after events refactoring

--- examples/meshPixarLight.jl
+++ examples/meshPixarLight.jl
@@ -9,7 +9,7 @@ using Rotations
 using StaticArrays
 
 WGPU.SetLogLevel(WGPU.WGPULogLevel_Off)
-canvas = WGPU.defaultCanvas(WGPU.WGPUCanvas; size=(500, 400));
+canvas = WGPU.defaultCanvas(WGPU.WGPUCanvas; size=(500, 500));
 gpuDevice = WGPU.getDefaultDevice();
 camera = defaultCamera()
 
@@ -24,93 +24,10 @@ scene = Scene(
 	repeat([nothing], 6)...
 )
 
-mesh = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "cube.obj"))
+mesh = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "pixarlamp.obj"))
 addObject!(scene, mesh)
 
-
-mutable struct MouseState
-	leftClick
-	rightClick
-	middleClick
-	prevPosition
-	speed
-end
-
-
-mouseState = MouseState(false, false, false, (0, 0), (0.01, 0.01))
-
-
-istruthy(::Val{GLFW.PRESS}) = true
-istruthy(::Val{GLFW.RELEASE}) = false
-
-
-function setMouseState(mouse, ::Val{GLFW.MOUSE_BUTTON_1}, state)
-	mouse.leftClick = istruthy(Val(state))
-end
-
-
-function setMouseState(mouse, ::Val{GLFW.MOUSE_BUTTON_2}, state)
-	mouse.rightClick = istruthy(Val(state))
-end
-
-
-function setMouseState(mouse, ::Val{GLFW.MOUSE_BUTTON_3}, state)
-	mouse.middleClick = istruthy(Val(state))
-	mat = Matrix{Float32}(I, (4, 4))
-	a = camera.uniformData
-	a[1:3, 1:3] = mat[1:3, 1:3]
-	camera.uniformData = a
-end
-
-
-WGPU.setMouseButtonCallback(
-	canvas, 
-	(_, button, action, a) -> begin
-		@info GLFW.MouseButton button action a mouseState
-		setMouseState(mouseState, Val(button), action)
-	end
-)
-
-
-WGPU.setScrollCallback(
-	canvas,
-	(_, xoff, yoff) -> begin
-		@info "MouseScroll" xoff, yoff
-		camera.scale = camera.scale .+ yoff.*maximum(mouseState.speed)
-	end
-)
-
-
-# TODO camera.up will be useful in reasonable movements
-WGPU.setCursorPosCallback(
-	canvas, 
-	(_, x, y) -> begin
-		@info "Mouse Position" x, y
-		if all(((x, y) .- canvas.size) .< 0)
-			if mouseState.leftClick
-				delta = (mouseState.prevPosition .- (x, y)).*mouseState.speed.*-1
-				@info delta
-				rot = RotXY((delta |> reverse)...)
-				mat = MMatrix{4, 4, Float32}(I)
-				mat[1:3, 1:3] = rot
-				camera.transform = camera.transform*mat 
-				mouseState.prevPosition = (x, y)
-			elseif mouseState.rightClick
-				delta = (mouseState.prevPosition .- (x, y)).*mouseState.speed
-				mat = MMatrix{4, 4, Float32}(I)
-				mat[1:3, 3] .= [delta..., 0]
-				camera.transform = camera.transform*mat
-				mouseState.prevPosition = (x, y)
-			elseif mouseState.middleClick
-				mat = MMatrix{4, 4, Float32}(I)
-				camera.transform = mat
-			else
-				mouseState.prevPosition = (x, y)
-			end
-		end
-	end
-)
-
+attachEventSystem(scene)
 
 # @enter	runApp(scene, gpuDevice, renderPipeline)
 main = () -> begin
--- examples/multiObject.jl
+++ examples/multiObject.jl
@@ -25,93 +25,12 @@ scene = Scene(
 	repeat([nothing], 6)...,
 )
 
-mesh1 = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "cube.obj"))
-mesh2 = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "cylinder.obj"))
+mesh1 = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "sphere.obj"))
+mesh2 = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "torus.obj"))
 addObject!(scene, mesh2)
 addObject!(scene, mesh1)
 
-
-mutable struct MouseState
-	leftClick
-	rightClick
-	middleClick
-	prevPosition
-	speed
-end
-
-
-mouseState = MouseState(false, false, false, (0, 0), (0.01, 0.01))
-
-
-istruthy(::Val{GLFW.PRESS}) = true
-istruthy(::Val{GLFW.RELEASE}) = false
-
-
-function setMouseState(mouse, ::Val{GLFW.MOUSE_BUTTON_1}, state)
-	mouse.leftClick = istruthy(Val(state))
-end
-
-
-function setMouseState(mouse, ::Val{GLFW.MOUSE_BUTTON_2}, state)
-	mouse.rightClick = istruthy(Val(state))
-end
-
-
-function setMouseState(mouse, ::Val{GLFW.MOUSE_BUTTON_3}, state)
-	mouse.middleClick = istruthy(Val(state))
-	mat = MMatrix{4, 4, Float32}(I)
-	camera.uniformData = mat
-end
-
-
-WGPU.setMouseButtonCallback(
-	canvas, 
-	(_, button, action, a) -> begin
-		@info GLFW.MouseButton button action a mouseState
-		setMouseState(mouseState, Val(button), action)
-	end
-)
-
-
-WGPU.setScrollCallback(
-	canvas,
-	(_, xoff, yoff) -> begin
-		@info "MouseScroll" xoff, yoff
-		camera.scale = camera.scale .+ yoff.*maximum(mouseState.speed)
-	end
-)
-
-
-# TODO camera.up will be useful in reasonable movements
-WGPU.setCursorPosCallback(
-	canvas, 
-	(_, x, y) -> begin
-		@info "Mouse Position" x, y
-		if all(((x, y) .- canvas.size) .< 0)
-			if mouseState.leftClick
-				delta = (mouseState.prevPosition .- (x, y)).*mouseState.speed
-				@info delta
-				rot = RotXY(delta...)
-				mat = MMatrix{4, 4, Float32}(I)
-				mat[1:3, 1:3] = rot
-				camera.transform = camera.transform*mat 
-				mouseState.prevPosition = (x, y)
-			elseif mouseState.rightClick
-				delta = (mouseState.prevPosition .- (x, y)).*mouseState.speed
-				mat = MMatrix{4, 4, Float32}(I)
-				mat[1:3, 3] .= [delta..., 0] .|> Float32
-				camera.transform = camera.transform*mat
-				mouseState.prevPosition = (x, y)
-			elseif mouseState.middleClick
-				mat = MMatrix{4, 4, Float32}(I)
-				camera.transform = mat
-			else
-				mouseState.prevPosition = (x, y)
-			end
-		end
-	end
-)
-
+attachEventSystem(scene)
 
 # @enter	runApp(scene, gpuDevice, renderPipeline)
 main = () -> begin
