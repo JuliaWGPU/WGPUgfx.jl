897d77ade75f380d717b9e912b7a625a1c4b1d4f
Arhik <arhik23@gmail.com>
2024-01-03 10:30:02 +0530

    first reasonable output

--- examples/splat.jl
+++ examples/splat.jl
@@ -11,13 +11,13 @@ using StaticArrays
 using WGPUNative
 using Images
 
-WGPUCore.SetLogLevel(WGPUCore.WGPULogLevel_Debug)
+WGPUCore.SetLogLevel(WGPUCore.WGPULogLevel_Off)
 
 scene = Scene()
 renderer = getRenderer(scene)
 
-# pc = defaultGSplat(joinpath(pkgdir(WGPUgfx), "assets", "bonsai", "bonsai_30000.ply"))
-pc = defaultGSplat(joinpath("C:\\", "Users", "arhik", "Downloads", "bonsai_30000.compressed.ply"))
+pc = defaultGSplat(joinpath(pkgdir(WGPUgfx), "assets", "bonsai", "bonsai_30000.ply"))
+# pc = defaultGSplat(joinpath("C:\\", "Users", "arhik", "Downloads", "bonsai_30000.compressed.ply"))
 
 addObject!(renderer, pc)
 
@@ -31,20 +31,7 @@ end
 
 mainApp = () -> begin
 	try
-		count = 0
-		camera1 = scene.cameraSystem[1]
 		while !WindowShouldClose(scene.canvas.windowRef[])
-			# rot = RotXY(0.01, 0.02)
-			# mat = MMatrix{4, 4, Float32}(I)
-			# mat[1:3, 1:3] = rot
-			# camera1.transform = camera1.transform*mat
-			# theta = time()
-			# pc.uniformData = translate((				
-			# 	1.0*(sin(theta)), 
-			# 	1.0*(cos(theta)), 
-			# 	0, 
-			# 	1
-			# )).linear
 			runApp(renderer)
 			PollEvents()
 		end
