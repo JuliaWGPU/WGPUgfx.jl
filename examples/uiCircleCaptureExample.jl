257795c47ec4e218c093646d5ceab6f3db513128
ut112002 <utmainmail@gmail.com@>
2024-03-07 16:02:39 +0530

    Minor fixes

--- examples/uiCircleCaptureExample.jl
+++ examples/uiCircleCaptureExample.jl
@@ -1,11 +1,3 @@
-# This is an incomplete example or WIP example.
-# Basic idea of this example is to showcase how Text can be rendered on Quads.
-# This will be a start for WGPU-UI code base once working and will be moved to WGPU-UI.
-
-# TODO this example should be able to get texture from bitmap using FreeType
-# And pass texture to Quad for UV coordinate mapping. End result should be a text rendered
-# on screen. This will be useful, lets say, for FPS display (frames per sencond).
-
 using Revise
 using WGPUgfx
 using WGPUCore
@@ -23,7 +15,7 @@ scene = Scene();
 renderer = getRenderer(scene);
 
 circle = defaultUICircle(;nSectors=20, radius=0.2, color = [0.4, 0.3, 0.5, 0.6]);
-#circle2 = defaultUICircle(;nSectors=100, radius=0.1, color = [0.5, 0.9, 0.2, 0.2]);
+circle2 = defaultUICircle(;nSectors=100, radius=0.1, color = [0.5, 0.9, 0.2, 0.2]);
 
 addObject!(renderer, circle, scene.camera);
 
@@ -48,7 +40,7 @@ main = () -> begin
 		flag = false
 		while !WindowShouldClose(scene.canvas.windowRef[])
 			runApp(renderer)
-	#=		if mouseState.leftClick == true
+			if mouseState.leftClick == true
 				if !flag
 					popfirst!(renderer.scene.objects)
 					addObject!(renderer, circle2, scene.camera)
@@ -60,7 +52,7 @@ main = () -> begin
 					addObject!(renderer, circle, scene.camera)
 					flag = false
 				end 
-			end =#
+			end 
 			@info keyboardState
 			PollEvents()
 		end
