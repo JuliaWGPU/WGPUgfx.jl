72a35d8fca27e813be57f05c860c37e21366bef6
Documenter.jl <documenter@juliadocs.github.io>
2024-03-07 14:35:42 +0000

    build based on 5fdd20f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WGPUgfx","category":"page"},{"location":"#WGPUgfx","page":"Home","title":"WGPUgfx","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WGPUgfx.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WGPUgfx]","category":"page"}]
+}
